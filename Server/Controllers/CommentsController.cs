bec9070bb47b9238c0e551650d369ca9daf07db4
Praveen P <praveen.padmanabhan@bhavnacorp.com>
2022-03-09 18:31:05 +0530

    Create, Get and update method

--- Server/Controllers/CommentsController.cs
+++ Server/Controllers/CommentsController.cs
@@ -38,7 +38,10 @@ namespace Server.Controllers
         {
         }
 
-      
-     
+        // DELETE api/<CommentsController>/5
+        [HttpDelete("{id}")]
+        public void DeleteCommentById(int id)
+        {
+        }
     }
 }
