a02763d959546dbb088a22ce620d11004c699cf0
Praveen P <praveen.padmanabhan@bhavnacorp.com>
2022-03-09 18:31:05 +0530

    Create, Get and update method

---
+++ Server/Controllers/CommentsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        // GET: api/<CommentsController>
+        [HttpGet("{id}")]
+        public IEnumerable<string> GetCommentsByPostId()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CommentsController>/5
+        [HttpGet("commentId/{id}")]
+        public string GetCommentByCommentId(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CommentsController>
+        [HttpPost]
+        public void CreateComment([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CommentsController>/5
+        [HttpPut("{id}")]
+        public void UpdateCommentById([FromBody] string value, int id)
+        {
+        }
+
+        // DELETE api/<CommentsController>/5
+        [HttpDelete("{id}")]
+        public void DeleteCommentById(int id)
+        {
+        }
+    }
+}
---
+++ Server/Controllers/SocialPostController.cs
@@ -0,0 +1,114 @@
+﻿using Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialPostController : ControllerBase
+    {
+        // GET: api/SocialPost
+        [HttpGet]
+        public IActionResult GetSocialPost()
+        {
+            //TODO: Have to write logic here
+            try
+            {
+                using (fbContext fb = new fbContext())
+                {
+
+
+                    return Ok(fb.Posts.ToList());
+                }
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // GET api/SocialPost/5
+        [HttpGet("{id}")]
+        public IActionResult GetSocialPostById(int id)
+        {
+            try
+            {
+                using (fbContext fb = new fbContext())
+                {
+
+                    return Ok(fb.Posts.First(x => x.PostId == id));
+                }
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // POST api/SocialPost
+        [HttpPost]
+        public IActionResult CreateSocialPost([FromBody] Post post)
+        {
+            try
+            {
+                using (fbContext fb = new fbContext())
+                {
+                    post.CreatedDate = DateTime.Now;
+                    post.UpdatedDate = null;
+                    post.Likes = 0;
+                    post.Hearts = 0;
+                    fb.Posts.Add(post);
+                    fb.SaveChanges();
+                    return Ok(post);
+                }
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // PUT api/SocialPost/5
+        [HttpPut("{id}")]
+        public IActionResult UpdateSocialPost(int id, [FromBody] Post post)
+        {
+            try
+            {
+                using (fbContext fb = new fbContext())
+                {
+                    var update = fb.Posts.First(x => x.PostId == id);
+                    update.PostName = post.PostName;
+                    update.PostDescription = post.PostDescription;
+                    update.UpdatedDate = DateTime.Now;
+                    fb.SaveChanges();
+                    return Ok(fb.Posts.First(x => x.PostId == id));
+                }
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPut("Like/{id}")]
+        public void UpdateSocialPostLike([FromBody] string value, int id)
+        {
+            //TODO: Have to write logic here
+        }
+
+        [HttpPut("Heart/{id}")]
+        public void UpdateSocialPostHeart([FromBody] string value, int id)
+        {
+            //TODO: Have to write logic here
+        }
+
+        
+    }
+}
